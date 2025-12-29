3f2ec955946af8ea1e888020b62b9b75135c060c
watersedgemanagement2023 <watersedgemanagement2023@gmail.com>
2025-12-29 04:16:23 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ import express from "express";
 import cors from "cors";
 import crypto from "crypto";
 import QRCode from "qrcode";
+import path from "path";
+import { fileURLToPath } from "url";
 import { initDb, getOrCreateCustomer, pool } from "./db.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "public")));
 
 await initDb();
 
