a4c4564b02a152c3fd6cad9a2448a353c727c2b4
watersedgemanagement2023 <watersedgemanagement2023@gmail.com>
2025-12-29 04:46:14 +0000

    Update server.js

--- server.js
+++ server.js
@@ -189,6 +189,10 @@ app.get("/api/me", async (req, res) => {
   res.json({ customer });
 });
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
